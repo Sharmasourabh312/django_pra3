14aa113b96ca0a3016467a369b0f847c896f232a
sourabh <saurabh@scip.co>
2024-02-01 15:30:04 +0530

    static files collect

--- testdj/urls.py
+++ testdj/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("mainapp.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns+=static(settings.STATIC_URL,documnet_root=settings.STATIC_ROOT)
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns+=staticfiles_urlpatterns()
+# if settings.DEBUG:
+#     urlpatterns+=static(settings.STATIC_URL,documnet_root=settings.STATIC_ROOT)
